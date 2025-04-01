2bca15af06a52e5561e43025f919a47507271bcd
ChanWeiKai0118 <m301111026@tmu.edu.tw>
2025-04-01 15:45:30 +0800

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,7 +1,7 @@
 import gspread
 import streamlit as st
 from google.oauth2.service_account import Credentials
-
+from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
 # 設定 Google Sheets API 權限
