e5afcef17c966376dee5b0fc58b0dccd8cc96663
ChanWeiKai0118 <m301111026@tmu.edu.tw>
2025-04-02 15:07:42 +0800

    Update UI.py

--- UI.py
+++ UI.py
@@ -36,10 +36,10 @@ def save_to_gsheet(data):
     row[55] = data[9]  # BD: aki_history
     
     # 在 A 欄插入 id_no 公式
-    row[0] = f'=IF(ROW()=2, 1, IF(COUNTIF(B$1:B2, B2) = 0, MAX(A$1:A2) + 1, IF(OR(H2<INDEX(H$1:H2, MAX(IF($B$1:B2=B2, ROW($B$1:B2)-1, 0))), I2<INDEX(I$1:I$2, MAX(IF($B$1:B2=B2, ROW($B$1:B2)-1, 0)))), MAX(A$1:A2) + 1, INDEX(A$1:A2, MAX(IF(B$1:B2=B2, ROW($B$1:B2)-1, 0))))))'
+    row[0] = f'=IF(ROW()=2, 1, IF(COUNTIF(B$1:B{last_row}, B{last_row}) = 0, MAX(A$1:A{last_row}) + 1, IF(OR(H{last_row}<INDEX(H$1:H{last_row}, MAX(IF($B$1:B{last_row}=B{last_row}, ROW($B$1:B{last_row})-1, 0))), I2<INDEX(I$1:I{last_row}, MAX(IF($B$1:B{last_row}=B{last_row}, ROW($B$1:B{last_row})-1, 0)))), MAX(A$1:A{last_row}) + 1, INDEX(A$1:A{last_row}, MAX(IF(B$1:B{last_row}=B{last_row}, ROW($B$1:B{last_row})-1, 0))))))'
 
     # 在 J 欄插入 treatment_duration 公式
-    row[9] = f'=IF(COUNTIF(A$2:A2, A2) = 1, 0, (F2 - INDEX(F$2:F$2, MATCH(A2, A$2:A2, 0)))/7)'
+    row[9] = f'=IF(COUNTIF(A$2:A{last_row}, A{last_row}) = 1, 0, (F{last_row} - INDEX(F$2:F{last_row}, MATCH(A{last_row}, A$2:A{last_row}, 0)))/7)'
 
     # 插入行資料
     sheet.append_row(row, value_input_option="USER_ENTERED")
