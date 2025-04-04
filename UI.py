568293e9371c9c70ebccfc1665b3898e7a5d28a5
ChanWeiKai0118 <m301111026@tmu.edu.tw>
2025-04-04 21:34:00 +0800

    Update UI.py

--- UI.py
+++ UI.py
@@ -36,7 +36,7 @@ def save_to_gsheet(data, sheet_name):
         row[11] = f'=IF(OR(G{last_row}=0, K{last_row}=0), 0, K{last_row} / G{last_row})'
         row[13] = f'=SUMIF(A$2:A{last_row}, A{last_row}, M$2:M{last_row})'
         row[14] = f'=IF(OR(H{last_row}=0, N{last_row}=0), 0, N{last_row} / H{last_row})'
-        row[16] = f'=IFNA(INDEX("lab data"!H:H, MATCH(1, ("lab data"!A:A = B{last_row}) * ("lab data"!E:E = MAX(FILTER("lab data"!E:E, ("lab data"!A:A = B{last_row}) * ("lab data"!E:E <= F{last_row}) * ("lab data"!E:E >= F{last_row} - 30) * ("lab data"!H:H <> "")))) * ("lab data"!H:H <> ""), 0)), "")'
+        row[16] = f'=IFNA(INDEX("lab data"!H:H, MATCH(1, ("lab data!A:A" = B{last_row}) * ("lab data!E:E" = MAX(FILTER("lab data!E:E", ("lab data!A:A" = B{last_row}) * ("lab data!E:E" <= F{last_row}) * ("lab data!E:E" >= F{last_row} - 30) * ("lab data!H:H" <> "")))) * ("lab data!H:H" <> ""), 0)), "")'
 
     
         sheet.append_row(row, value_input_option="USER_ENTERED")
