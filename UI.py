9c4837a667309d766acfbc56894f37e29f0254c4
ChanWeiKai0118 <m301111026@tmu.edu.tw>
2025-04-04 21:56:20 +0800

    Update UI.py

--- UI.py
+++ UI.py
@@ -14,8 +14,8 @@ def get_gsheet_client():
 def save_to_gsheet(data, sheet_name):
     client = get_gsheet_client()
     
-    if sheet_name == "chemo data":
-        sheet = client.open("web data").worksheet("chemo data")
+    if sheet_name == "chemo_data":
+        sheet = client.open("web data").worksheet("chemo_data")
         row = ["" for _ in range(57)]  
         row[1], row[3], row[2], row[4], row[5] = data[0], data[1], data[2], data[3], data[4]
     
@@ -36,20 +36,20 @@ def save_to_gsheet(data, sheet_name):
         row[11] = f'=IF(OR(G{last_row}=0, K{last_row}=0), 0, K{last_row} / G{last_row})'
         row[13] = f'=SUMIF(A$2:A{last_row}, A{last_row}, M$2:M{last_row})'
         row[14] = f'=IF(OR(H{last_row}=0, N{last_row}=0), 0, N{last_row} / H{last_row})'
-        row[16] = f'=IFNA(INDEX("lab data!H:H", MATCH(1, ("lab data!A:A" = B{last_row}) * ("lab data!E:E" = MAX(FILTER("lab data!E:E", ("lab data!A:A" = B{last_row}) * ("lab data!E:E" <= F{last_row}) * ("lab data!E:E" >= F{last_row} - 30) * ("lab data!H:H" <> "")))) * ("lab data!H:H" <> ""), 0)), "")'
+        row[16] = f'=IFNA(INDEX(lab_data!H:H, MATCH(1, (lab_data!A:A = B{last_row}) * (lab_data!E:E = MAX(FILTER(lab_data!E:E, (lab_data!A:A = B{last_row}) * (lab_data!E:E <= F{last_row}) * (lab_data!E:E >= F{last_row} - 30) * (lab_data!H:H <> "")))) * (lab_data!H:H <> ""), 0)), "")'
 
     
         sheet.append_row(row, value_input_option="USER_ENTERED")
 
-    elif sheet_name == "lab data":
-        sheet = client.open("web data").worksheet("lab data")
+    elif sheet_name == "lab_data":
+        sheet = client.open("web data").worksheet("lab_data")
         last_row = len(sheet.get_all_values()) + 1
         row = ["" for _ in range(14)]  
         
         row[0], row[3], row[4] = data[0], data[1], data[2]
         row[6], row[7], row[11], row[12], row[13] = data[3], data[4], data[5], data[6], data[7]
-        row[1] = f'=IFERROR(VLOOKUP(A{last_row}, INDIRECT("chemo data!B:D"), 3, FALSE), "")'  # 查找性别
-        row[2] = f'=IFERROR(VLOOKUP(A{last_row}, INDIRECT("chemo data!B:E"), 4, FALSE), "")'  # 查找年紀
+        row[1] = f'=IFERROR(VLOOKUP(A{last_row}, INDIRECT("chemo_data!B:D"), 3, FALSE), "")'  # 查找性别
+        row[2] = f'=IFERROR(VLOOKUP(A{last_row}, INDIRECT("chemo_data!B:E"), 4, FALSE), "")'  # 查找年紀
         # F 列: 如果 G (BUN) 有值，則填入 G，否則找最近的 BUN
         row[5] = f'=IF(G{last_row}<>"", G{last_row}, IF(ROW()=2, "", IFERROR(INDEX(G$2:G{last_row-1}, MAX(IF(A$2:A{last_row-1}=A{last_row}, ROW(A$2:A{last_row-1})-1, 0))), "")))'
 
